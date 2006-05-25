30c1d676cdeeafa709dd82907f8dcc56df3a5144
ynakam <ynakam@d001d537-7013-0410-b331-b905d683b49c>
2006-05-25 02:55:12 +0000

    =

---
+++ seedit-converter/utils/seedit-rbac.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+POLICY_ROOT=/etc/seedit/policy/
+
+
+cd $POLICY_ROOT
+pwd
+
+if [ $1 = "on" ]; then
+    echo "Enabling RBAC"
+    echo "Going to permissive mode"
+    /usr/sbin/setenforce 0
+    mv extras/*_r.sp .
+    mv extras/su_t.sp .
+    mv extras/newrole_t.sp .
+    mv unconfined_t.sp extras
+    mv unconfined_su_t.sp extras
+    /usr/sbin/seedit-load -v
+    echo "done. Reboot"
+fi
+
+if [ $1 = "off" ]; then
+    echo "Disabling RBAC"
+    echo "Going to permissive mode"
+    /usr/sbin/setenforce 0
+    mv *_r.sp extras
+    mv su_t.sp extras
+    mv newrole_t.sp extras
+    mv extras/unconfined_t.sp .
+    mv extras/unconfined_su_t.sp .
+    /usr/sbin/seedit-load -v
+    echo "done, reboot"
+fi
+
+

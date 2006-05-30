8c677c4850e9f3b07809270b0b1a95182ccd1a2f
ynakam <ynakam@d001d537-7013-0410-b331-b905d683b49c>
2006-05-30 19:37:50 +0000

    =

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ mkdir -p archive
 cd archive
 rpmbuild -ta seedit-converter-$VERSION.tar.gz 
 rpmbuild -ta seedit-policy-$VERSION.tar.gz 
-mv ~/rpm/RPMS/i386/seedit-converter-$VERSION-1.i386.rpm .
+mv ~/rpm/RPMS/i386/seedit-converter-$VERSION-FC5.i386.rpm .
 mv ~/rpm/RPMS/noarch/seedit-policy-$VERSION-FC5.noarch.rpm .
-mv ~/rpm/SRPMS/seedit-converter-$VERSION-1.src.rpm .
+mv ~/rpm/SRPMS/seedit-converter-$VERSION-FC5.src.rpm .
 mv ~/rpm/SRPMS/seedit-policy-$VERSION-FC5.src.rpm .
 
 

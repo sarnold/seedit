2fc27cae089bf56f69b8f36ad2f22d8e9fdea9ad
ynakam <ynakam@d001d537-7013-0410-b331-b905d683b49c>
2006-06-09 04:07:20 +0000

    =

--- seedit-gui/seedit-gui-generate-policy.py
+++ seedit-gui/seedit-gui-generate-policy.py
@@ -23,12 +23,16 @@ class generatePolicyThread(threading.Thread):
 		(i,size)=data
 		self.mDialog.mProgressLabel.set_text(_("Progress:%d/%d") % (i,size))	
 
+	def emit_done(self,result):
+		self.mDialog.mWindow.emit("done",result)
+	
 	
 	def __init__(self,dialog):
 		threading.Thread.__init__(self)
 		self.mDialog = dialog
 	def run(self):
-		self.mDialog.mGenerateButton.set_sensitive(False)
+	
+		gobject.idle_add(self.mDialog.mGenerateButton.set_sensitive,False)
 
 		domdoc=readSPDLSpec(gSpecXML)
 		result =[]
@@ -40,7 +44,6 @@ class generatePolicyThread(threading.Thread):
 		for line in lines:
 			data = (i,size)
 			gobject.idle_add(self.updateLabel, data)
-			print i
 			rule = parseLine(line)
 			if(rule):
 				spRuleList=genSPDL(rule,line,domdoc)
@@ -50,8 +53,12 @@ class generatePolicyThread(threading.Thread):
 			i = i+1				     
 		data = (i,size)
 		gobject.idle_add(self.updateLabel, data)
-		self.mDialog.mWindow.emit("done",result)
-		self.mDialog.mGenerateButton.set_sensitive(True)
+		gobject.idle_add(self.mDialog.mProgressLabel.set_text,_("Done"))
+		gobject.idle_add(self.mDialog.mGenerateButton.set_sensitive,True)
+		#emit "done" signal here!
+		gobject.idle_add(self.emit_done,result)
+
+	
 
 
 class seeditGeneratePolicyWindow(seeditCommon):
@@ -60,13 +67,92 @@ class seeditGeneratePolicyWindow(seeditCommon):
 		thread = generatePolicyThread(self)
 		thread.start()
 
+        ## to avoid suggesting same rules
+	def mergeSamePolicy(self, list):
+		result=[]
+		outputStr = dict()
+		for l in list:
+			(domain, log, allow) = l
+			key = domain+allow
+
+			if not outputStr.has_key(key):
+				outputStr[key]=[]
+			a = (domain, allow , log)
+			outputStr[key].append(a)
+
+		for key in  outputStr.keys():
+			list = outputStr[key]
+			log=""
+			for l in list:
+				domain = l[0]
+				allow = l[1]
+				log=log+l[2]
+				
+			result.append((domain,allow,log))
+
+		return result
 	'''
 	When policy generation is done, it is called
 	'''
         def doneCallBack(self,widget,data=None):
-		print data
+		treeview = self.mGeneratedPolicyTreeView
+		model = treeview.get_model()
+		model.clear()
+
+		lists = data
+		list =[]
+		for l in lists:
+			for ll in l:
+				list.append(ll)
+		list = self.mergeSamePolicy(list)
+
+		for l in list:
+			(domain,allow,log)=l			
+			appended=(domain,allow,log)
+			self.mGeneratedPolicyListStore.append(appended)
+	
+		return
+	
+	def addColumns(self, treeview, header):
+		model = treeview.get_model()
+        
+
+		renderer = gtk.CellRendererToggle()
+
+		column = gtk.TreeViewColumn(header[0], gtk.CellRendererText(),
+					    text=0)
+		column.set_sort_column_id(0)
+		treeview.append_column(column)
+
+
+		column = gtk.TreeViewColumn(header[1], gtk.CellRendererText(),
+					    text=1)
+		column.set_sort_column_id(1)
+		treeview.append_column(column)
+		column = gtk.TreeViewColumn(header[2], gtk.CellRendererText(),
+					    text=2)
+		column.set_sort_column_id(2)
+		treeview.append_column(column)
+        
+	def initTreeView(self,model,header):
+		sw = gtk.ScrolledWindow()
+		sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+		sw.set_size_request(300,300)
+		treeview = gtk.TreeView(model)
+
+		treeview.set_rules_hint(True)
+		treeview.set_search_column(2)
+		sw.add(treeview)
+		self.addColumns(treeview,header)
+		return (sw,treeview)
 
         def __init__(self):
+		self.mGeneratedPolicyListStore = gtk.ListStore(
+			gobject.TYPE_STRING,				
+			gobject.TYPE_STRING,
+			gobject.TYPE_STRING)
+
+		
 		gobject.signal_new("done", gtk.Window,
 				   gobject.SIGNAL_RUN_LAST,
 				   gobject.TYPE_NONE,
@@ -78,6 +164,16 @@ class seeditGeneratePolicyWindow(seeditCommon):
 		window.connect('destroy', lambda w: gtk.main_quit())
 		vbox = gtk.VBox()
 		window.add(vbox)
+
+		header = (_("Domain"),_("Policy"),_("Log"))
+		(sw,tv) = self.initTreeView(self.mGeneratedPolicyListStore,header)
+		self.mGeneratedPolicyTreeView=tv
+
+		appended=("","","")
+		self.mGeneratedPolicyListStore.append(appended)
+
+		vbox.pack_start(sw,False,False,5)
+		
 		button = gtk.Button(_("Generate"))
 		vbox.pack_start(button, False, False, 5)
 		button.connect("clicked", self.generateCallBack)

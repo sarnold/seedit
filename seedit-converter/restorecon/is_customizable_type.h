31d7b6cab9dc164e98e95448c9d0783ccdd3c7f8
ynakam <ynakam@d001d537-7013-0410-b331-b905d683b49c>
2006-06-16 15:40:32 +0000

    =

--- seedit-converter/restorecon/is_customizable_type.h
+++ seedit-converter/restorecon/is_customizable_type.h
@@ -1,7 +1,9 @@
+#ifdef COS4
+#define NO_CUSTOMIZABLE_TYPES
+#endif
 #ifdef AX2
 #define NO_CUSTOMIZABLE_TYPES
 #endif
-
 
 #ifdef NO_CUSTOMIZABLE_TYPES
 int is_context_customizable(security_context_t context);

5f3dcd8a82b6a134e4128b8a64d92ffb96fcbccf
ynakam <ynakam@d001d537-7013-0410-b331-b905d683b49c>
2006-10-25 23:28:28 +0000

    =

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
-VERSION=2.1.0.b1
+VERSION=2.1.0.b3
 RELEASE=1
-DISTRO=COS4
+DISTRO=FC6
+AUDITCONF="/etc/audit/audit.conf"
 
 mkdir -p archive/source
 mkdir -p archive/RPM
 
 cd archive
-rpmbuild -ta seedit-converter-$VERSION-$RELEASE.tar.gz 
-rpmbuild -ta seedit-policy-$VERSION-$RELEASE.tar.gz 
-rpmbuild -ta seedit-gui-$VERSION-$RELEASE.tar.gz 
+rpmbuild -ta seedit-converter-$VERSION-$RELEASE.tar.gz --define "DISTRO=$DISTRO"
+rpmbuild -ta seedit-policy-$VERSION-$RELEASE.tar.gz --define "DISTRO=$DISTRO" --define "AUDITCONF=$AUDITCONF"
+rpmbuild -ta seedit-gui-$VERSION-$RELEASE.tar.gz --define "DISTRO=$DISTRO"
 #rpmbuild -ta seedit-doc-$VERSION-$RELEASE.tar.gz 
 
 mv ~/rpm/RPMS/i386/seedit-converter-$VERSION-$RELEASE.$DISTRO.i386.rpm ./RPM

5354575a2f8b202191d5634645c7a435384edfc8
ynakam <ynakam@d001d537-7013-0410-b331-b905d683b49c>
2006-11-10 02:51:58 +0000

    =

--- buildpkg.sh
+++ buildpkg.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-. common.sh
+#Fix these valuables for your environment
+VERSION=2.1.0
+BETA=-beta4
+DISTRO=FC6
+RELEASE=0
+PYTHON_VER=2.4
+AUDITCONF=\\/etc\\/audit\\/audit.rules
+MODULAR=y
+CUSTOMIZABLE_TYPES=y
+SVNROOT=~/seedit/trunk/
 
 mkdir -p archive
 
